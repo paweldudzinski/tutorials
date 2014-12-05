b194cf3519550e8f12bbccf140a92ba212f10cc9
Paweł Dudziński <pawel.d@webinterpret.com>
2014-12-05 15:25:40 +0100

    Bugfix

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from bottle import route, run, template, redirect, static_file, get_url
+from bottle import route, run, template, redirect, static_file
+from bottle import Bottle
 
 @route('/static/:path#.+#', name='static')
 def static(path):
@@ -11,6 +12,6 @@ def index():
 @route('/erlang-python')
 @view('erlang_python')
 def index():
-    return { 'get_url': get_url } 
+    return { 'get_url': Bottle.get_url } 
 
 run(host='127.0.0.1', port=5010)
