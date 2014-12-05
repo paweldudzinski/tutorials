7e143ba4e5e4be033900596a8b2601814217d98a
Paweł Dudziński <pawel.d@webinterpret.com>
2014-12-05 15:22:14 +0100

    Static fies

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
-from bottle import route, run, template, redirect
+from bottle import route, run, template, redirect, static_file, get_url
+
+@route('/static/:path#.+#', name='static')
+def static(path):
+    return static_file(path, root='static')
 
 @route('/')
 def index():
     return redirect('/erlang-python')
 
 @route('/erlang-python')
+@view('erlang_python')
 def index():
-    return template('erlang_python')
+    return { 'get_url': get_url } 
 
 run(host='127.0.0.1', port=5010)
